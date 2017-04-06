e30bb8ef78c5ebd65ac9de469604c80fee1966b9
juanma-cvega <juanma.cvega@gmail.com>
2017-04-06 19:05:00 +0200

    Renamed SmokeTest to SmokeITest. Added failsafe plugin. Configured integration-tests maven profile to run the smoke tests and skip unit tests. Small changes in pom file.

--- test/src/test/java/com/jusoft/component/slot/SmokeITest.java
+++ test/src/test/java/com/jusoft/component/slot/SmokeITest.java
@@ -6,13 +6,10 @@ import io.restassured.http.ContentType;
 import org.junit.Test;
 
 @Slf4j
-public class SmokeTest {
-
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SmokeTest.class);
+public class SmokeITest {
 
     @Test
     public void test() {
-        log.info("estoy aqui");
         RestAssured.baseURI = "http://192.168.99.100";
         RestAssured.port = 8080;
         RestAssured.get("/bookings/user/1").then().contentType(ContentType.JSON);
